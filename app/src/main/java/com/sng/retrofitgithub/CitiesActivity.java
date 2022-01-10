40f3fe21a0a91e454f4d096b78061935fcd4b2ff
Gabriel Nunez <arielygabo@gmail.com>
2022-01-10 12:41:39 +0300

    First commit

---
+++ app/src/main/java/com/sng/retrofitgithub/CitiesActivity.java
@@ -0,0 +1,14 @@
+package com.sng.retrofitgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cities);
+    }
+}
---
+++ app/src/main/java/com/sng/retrofitgithub/PlaceholderApi.java
@@ -0,0 +1,12 @@
+package com.sng.retrofitgithub;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PlaceholderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
